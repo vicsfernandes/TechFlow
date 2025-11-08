b9793dc726a85e80c6e1c93426b205f520f47e9c
Vic <victorsfernandes18@gmail.com>
2025-11-08 12:56:48 -0300

    Testes para cenário válido e inválido

---
+++ tests/test_app.py
@@ -0,0 +1,24 @@
+import pytest
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
+
+from app import app as flask_app
+
+@pytest.fixture
+def client():
+    with flask_app.test_client() as client:
+        yield client
+
+def test_sku_valido(client):
+    response = client.post('/', data={'sku': 'RTX-4090'}, follow_redirects=True)
+    
+    assert response.status_code == 200
+    assert b'SKU: RTX-4090 | Quantidade em Estoque: 150 unidades.' in response.data
+
+def test_sku_invalido(client):
+    response = client.post('/', data={'sku': 'SKU-FALSO'}, follow_redirects=True)
+    
+    assert response.status_code == 200
+    assert b'SKU SKU-FALSO nao encontrado no inventario.' in response.data
